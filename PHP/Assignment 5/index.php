17fabd908dc3fa6cce69564a10be48947b51ccc9
beingsohan <syedsohan1995@gmail.com>
2020-08-08 23:58:41 +0600

    Assignments of Building Web Applications in PHP course added.

---
+++ PHP/Assignment 5/index.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Md. Morsalat Hasan PHP</title>
+</head>
+<body>
+<h1>Md. Morsalat Hasan PHP</h1>
+<p>The SHA256 hash of "Md. Morsalat Hasan" is 
+<?php echo hash('sha256', 'Md. Morsalat Hasan'); ?></p>
+<pre>ASCII ART:
+
+    **         **
+    ** *     * **
+    **    *    **
+    **         **
+    **         **
+    **         **
+    
+</pre>
+<a href="check.php">Click here to check the error setting</a>
+<br/>
+<a href="fail.php">Click here to cause a traceback</a>
+</body>
